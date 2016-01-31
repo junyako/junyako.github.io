2d51176a149afa97a417bec7d54932fdd77301ad
junyako <kobayashijunya@kobayashijunya-no-MacBook-Air.local>
2016-01-31 15:05:21 +0900

    a

--- device/js/devicemotion.js
+++ device/js/devicemotion.js
@@ -1,18 +1,31 @@
 $(function (){
-    window.addEventListener('devicemotion',shakeDevice);
 
+    //加速度のイベント
+    window.addEventListener('devicemotion',shakeDevice);
     function shakeDevice(evt){
         var x = evt.acceleration.x;
         var y = evt.acceleration.y;
         var z = evt.acceleration.z;
 
-        $('#device li').eq(0).text('加速度' +  x);
-        $('#device li').eq(1).text('加速度' +  y);
-        $('#device li').eq(2).text('加速度' +  z);
+        $('#device li').eq(0).text('加速度x' +  x);
+        $('#device li').eq(1).text('加速度y' +  y);
+        $('#device li').eq(2).text('加速度z' +  z);
 
         if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15) {
             $('#result').text('振ったね');
         }
+    }
 
+    //傾き
+    window.addEventListener('deviceorientation',roteDevice);
+
+    function roteDevice(evt) {
+        var x = evt.beta;
+        var y = evt.gamma;
+        var z = evt.alpha;
+
+        $('#device li').eq(0).text('傾きx' +  x);
+        $('#device li').eq(1).text('傾きy' +  y);
+        $('#device li').eq(2).text('傾きz' +  z);
     }
 });
