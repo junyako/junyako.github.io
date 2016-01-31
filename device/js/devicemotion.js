3fa5ffd5803e9f0731c70a4478c3957f588f7a0e
junyako <kobayashijunya@kobayashijunya-no-MacBook-Air.local>
2016-01-31 14:29:13 +0900

    a

---
+++ device/js/devicemotion.js
@@ -0,0 +1,14 @@
+$(function (){
+    window.addEventlistener('devicemotion',shakeDevice);
+
+    function shakeDevice(evt){
+        var x = evt.acceleration.x;
+        var y = evt.acceleration.y;
+        var Z = evt.acceleration.z;
+
+        $('#device li').eq(0).text('加速度' +  x);
+        $('#device li').eq(1).text('加速度' +  y);
+        $('#device li').eq(2).text('加速度' +  z);
+
+    }
+});
